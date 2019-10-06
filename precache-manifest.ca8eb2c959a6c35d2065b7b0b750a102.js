a87cf71a576424fc9f46f72104114c4688ba2170
Manish <manis.kasyap@gmail.com>
2019-10-06 13:40:39 +0400

    Updates

---
+++ precache-manifest.ca8eb2c959a6c35d2065b7b0b750a102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc7d18b952469b7211d51827d013ab5",
+    "url": "/superheroes-hooked/index.html"
+  },
+  {
+    "revision": "8bfcdd286165bad9e322",
+    "url": "/superheroes-hooked/static/css/main.717d7d4e.chunk.css"
+  },
+  {
+    "revision": "b17c1e9de101e7e42282",
+    "url": "/superheroes-hooked/static/js/2.175a135e.chunk.js"
+  },
+  {
+    "revision": "8bfcdd286165bad9e322",
+    "url": "/superheroes-hooked/static/js/main.5a0d292d.chunk.js"
+  },
+  {
+    "revision": "cc56ee7901ed5aa5e8a8",
+    "url": "/superheroes-hooked/static/js/runtime-main.1a71840c.js"
+  }
+]);
